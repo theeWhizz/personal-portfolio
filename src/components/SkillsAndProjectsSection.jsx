a6bcbf954c1cc6355e6584a5d20bd8e7ddde47ec
theeWhizz <theewhizz2@gmail.com>
2025-03-20 04:58:36 -0700

    s/p

--- src/components/SkillsAndProjectsSection.jsx
+++ src/components/SkillsAndProjectsSection.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useRef } from "react"
+
 import Slider from "react-slick"
 import { Code, Terminal, Database, Laptop, Layout, Palette, Github, ExternalLink } from "lucide-react"
 
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-import { useEffect, useRef } from "react"
 
 const skills = [
   {
